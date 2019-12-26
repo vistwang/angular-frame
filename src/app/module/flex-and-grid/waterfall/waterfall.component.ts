2f48c44c80b00753cfff976fa461e1729398a51a
vist wang <vistwang@gmail.com>
2019-12-26 16:31:18 +0800

    no message

--- src/app/module/flex-and-grid/waterfall/waterfall.component.ts
+++ src/app/module/flex-and-grid/waterfall/waterfall.component.ts
@@ -17,7 +17,15 @@ export class WaterfallComponent implements OnInit {
     return Math.floor(Math.random()*(100 - 400) + 400) + 'px';
   }
   renderColor() {
-    return '#' + Math.floor(Math.random()*(2<<23)).toString(16);
+    // return '#' + Math.floor(Math.random()*(2<<23)).toString(16);
+
+    return '#'+(Math.random()*0xffffff<<0).toString(16);
+
+    // return  '#' + ((color) => {
+    //   return (color +=  '0123456789abcdef'[Math.floor(Math.random()*16)])
+    //     && (color.length == 6) ?  color : arguments.callee(color);
+    //   })('');
+    // }
   }
 
 }
